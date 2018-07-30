b7675bd63232a646dc1f6082a43ed9648b5619c9
dylanneve1 <dylanneve1@gmail.com>
2018-07-30 12:28:28 +0100

    Add comments and clean up

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,31 +1,42 @@
-import java.util.Arrays;
+// Written by Dylan Neve.
 
+import java.util.Arrays;
 
 public class BubbleSort {
 	
+	// Variables needed for algorithm.
 	static int currentNum;
 	static int nextNum;
 	static int biggerNum;
 	static int smallerNum;
-
+	
+	// Array to sort.
 	static int numbers[] = {11, 5, 9, 1};
+	
+	// Get size for for loop.
 	static int size = (numbers.length) - 2;
+	
+	// Set step value as 1.
 	static int step = 1;
 	
+	// Create boolean for whether or not the arrangement of numbers was changed.
 	static boolean swapped = true;
 	
 	public static void main(String[] args) {
     
 		for(int i = 0; i <= size; i++) {
 			
+			// Set current number and next number values.
 			currentNum = numbers[i];
 			nextNum = numbers[i+1];
 			
-			if(swapped == true) {
+			// If arrangement has been changed print step and +1 to step value.
+			if(swapped) {
 				System.out.println("\nStep : " + step);
 				System.out.println(Arrays.toString(numbers));
 			}
 			
+			// Check whether current or next value is bigger.
 			if(currentNum > nextNum) {
 				biggerNum = currentNum;
 				smallerNum = nextNum;
@@ -36,9 +47,12 @@ public class BubbleSort {
 				smallerNum = currentNum;
 				swapped = false;
 			}
+			
+			// Write result to array.
 			numbers[i] = smallerNum;
 			numbers[i + 1] = biggerNum;
 			
+			// Loop back if end of array is reached.
 			if(i == size) {
 				i = -1;
 			}
